9221cd4a53c3f1d9cc52019b2bee4ea2298bb8f1
Matéo VIEL <mateo.viel@epitech.eu>
2021-08-07 17:01:29 +0200

    [FIX] fixed underflow

--- challenges/ultimate/contracts/Underflow.sol
+++ challenges/ultimate/contracts/Underflow.sol
@@ -5,30 +5,29 @@ contract Underflow {
     address private owner;
     int8 public currentWaterAmount = 0;
     int8 targetWaterAmount = 100;
-    
+
     constructor() public payable {
         owner = msg.sender;
     }
-    
+
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-    
+
     function giveWaterToGrumpy(int8 _waterAmount) public {
-        
+
         int8 maximumAmountGiven = 20;
 
         require(_waterAmount < maximumAmountGiven, "Too many water for grumpy");
-        require(currentWaterAmount + _waterAmount < targetWaterAmount, "Too less water for grumpy");
-        
+        require(int16(currentWaterAmount) +  int16(_waterAmount) < int16(targetWaterAmount));
+
         currentWaterAmount += _waterAmount;
-        
+
     }
-    
 
     function claim() public onlyOwner returns (bool) {
-        
+
         if (currentWaterAmount == targetWaterAmount)
             return (true);
         else
