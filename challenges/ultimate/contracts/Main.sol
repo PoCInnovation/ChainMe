ac610f5d48614ffdf3ddd1270df12abc9b43e693
Lucas <72020723+lucas-louis@users.noreply.github.com>
2021-08-07 18:40:52 +0200

    [UP] update with deploy contract and new system of checking

--- challenges/ultimate/contracts/Main.sol
+++ challenges/ultimate/contracts/Main.sol
@@ -1,26 +1,58 @@
 pragma solidity ^0.6.0;
 
+import "./Subway.sol";
+import "./House.sol";
+import "./ItSDoneIn2Sec.sol";
+import "./Gumpy.sol";
+
 contract Main {
 
     address private owner;
+    Subway private subwayContract;
+    House private houseContract;
+    ItSDoneIn2Sec private easyContract;
+    Gumpy private gumpyContract;
+    uint8 public count = 0;
+    uint8 [4] public order = [0, 0, 0, 0];
 
     constructor() public payable {
+        require(msg.value == 0.001 ether, "Not enough ether");
         owner = msg.sender;
+        subwayContract = (new Subway).value(0.001 ether)(address(this));
+        houseContract = (new House)(0x48692c2074686973206973206d79206c6f636b656420686f7573652120203a29, address(this), 1 ether);
+        easyContract = (new ItSDoneIn2Sec)(address(this));
+        gumpyContract = (new Gumpy)(address(this));
+    }
+
+    modifier onlyOwner() {
+        require(tx.origin == owner);
+        _;
     }
 
     function claim() public {
-        uint8 count = 0;
-        uint8 [10] memory order = [2, 3, 5, 9, 8, 1, 7, 4, 0, 6];
-        address [10] memory adresses = [];
+        uint8 [4] memory realOrder = [1, 3, 0, 2];
+        //address [4] memory adresses = [address(subwayContract), address(houseContract), address(easyContract), address(gumpyContract)];
 
-        for (int i = 0; i < 10; ++i) {
-            if (adresses[order[i]].call(abi.encodeWithSignature("claim()")) == true)
+        /*count = 0;
+        for (uint i = 0; i < 4; ++i) {
+            (bool result, bytes memory data) = adresses[order[i]].call(abi.encodeWithSignature("claim()"));
+            if (result == true)
                 count++;
             else
                 count = 0;
         }
-        require(count == 10);
+        require(count == 4);
+        owner = msg.sender;*/
+        for (uint i = 0; i < 4; i++) {
+            require(order[i] == realOrder[i], "Bad order");
+        }
         owner = msg.sender;
+    }
+    
+    function ilfautunnom(uint8 idChall) public onlyOwner {
+        require(count < 4, "C'est la merde !!");
+        order[count] = idChall;
+        count++;
     }
 
 }
