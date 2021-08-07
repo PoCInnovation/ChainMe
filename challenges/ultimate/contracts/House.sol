3eda342178aed1dee851ff8672632bdf4fe92f38
Lucas <72020723+lucas-louis@users.noreply.github.com>
2021-08-07 18:42:28 +0200

    [UP] update claiming system & add function for retrieve all the ether

--- challenges/ultimate/contracts/House.sol
+++ challenges/ultimate/contracts/House.sol
@@ -3,29 +3,28 @@ pragma solidity ^0.6.0;
 contract House {
     
     address private owner;
+    address private mainContract;
     uint256 private etherAmount;
     bool public lockedHouse = true;
     bytes32 private password;
     
-    constructor(bytes32 _password) public payable {
+    constructor(bytes32 _password, address _mainContract, uint256 _etherAmount) public {
         password = keccak256(abi.encodePacked(_password));
-        etherAmount = msg.value;
+        etherAmount = _etherAmount;
         owner = msg.sender;
-    }
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+        mainContract = _mainContract;
     }
     
     function openTheHouse(bytes32 _password) public payable {
         require(msg.value == etherAmount);
-        if (keccak256(abi.encodePacked(_password)) == password)
+        if (keccak256(abi.encodePacked(_password)) == password) {
             lockedHouse = false;
+            address(mainContract).call(abi.encodeWithSignature("ilfautunnom(uint8)", 1));
+        }
     }
     
-    function claim() external onlyOwner view returns (bool) {
-        return (! lockedHouse);
+    function tupeuxenvoyerauxautres(address payable _lesautres) public {
+        _lesautres.transfer(address(this).balance);
     }
     
 }
