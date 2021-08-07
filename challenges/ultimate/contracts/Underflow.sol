a910262ec10865ba7ff1bada3569ec9453be1866
Matéo VIEL <mateo.viel@epitech.eu>
2021-08-07 16:51:34 +0200

    [UPDATE] updated underflow to make it possible

--- challenges/ultimate/contracts/Underflow.sol
+++ challenges/ultimate/contracts/Underflow.sol
@@ -3,31 +3,32 @@ pragma solidity ^0.6.0;
 contract Underflow {
 
     address private owner;
-    int8 currentWaterAmount = 0;
-    int16 targetWaterAmount = 32000;
-
+    int8 public currentWaterAmount = 0;
+    int8 targetWaterAmount = 100;
+    
     constructor() public payable {
         owner = msg.sender;
     }
-
+    
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-
+    
     function giveWaterToGrumpy(int8 _waterAmount) public {
-
+        
         int8 maximumAmountGiven = 20;
 
         require(_waterAmount < maximumAmountGiven, "Too many water for grumpy");
-
+        require(currentWaterAmount + _waterAmount < targetWaterAmount, "Too less water for grumpy");
+        
         currentWaterAmount += _waterAmount;
-
+        
     }
+    
 
-
-    function claim() external onlyOwner returns (bool) {
-
+    function claim() public onlyOwner returns (bool) {
+        
         if (currentWaterAmount == targetWaterAmount)
             return (true);
         else
