5a2e88c74d8adf86b010be8abf8f501364603544
Matéo VIEL <mateo.viel@epitech.eu>
2021-08-28 15:06:03 +0200

    [ADD] added utils function to deploy a contract

---
+++ tests_and_init/utils/deployContract.js
@@ -0,0 +1,17 @@
+const web3 = require('../utils/connection');
+const compileContract = require('../utils/compileContract');
+const sendTransaction = require('../utils/sendTransaction');
+
+async function deployContract(sources, fileName, contractName, value) {
+    let contractFile = compileContract(sources, fileName, contractName);
+    const bytecode = contractFile.evm.bytecode.object;
+    const abi = contractFile.abi;
+
+    console.log('Attempting to deploy from account:', process.env.ADDRESS);
+    const incrementer = new web3.eth.Contract(abi);
+    const incrementerTx = incrementer.deploy({ data: bytecode });
+    let createReceipt = await sendTransaction(null, value, '3000000', incrementerTx.encodeABI());
+    console.log('Contract deployed at address', createReceipt.contractAddress);
+}
+
+module.exports = deployContract;
