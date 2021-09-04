b41132187ea6161d9620e3a20309c05428b0707b
Lucas <72020723+lucas-louis@users.noreply.github.com>
2021-09-04 15:33:46 +0200

    [UP] remove import SafeMath

--- contracts/levels/ultimate/Subway.sol
+++ contracts/levels/ultimate/Subway.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.6.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/math/SafeMath.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/math/SafeMath.sol";
 //import '@openzeppelin/contracts/math/SafeMath.sol';
 
 contract Subway {
     
-    using SafeMath for uint256;
+    //using SafeMath for uint256;
     address private owner;
     address private mainContract;
     uint256 public ticketPrice;
@@ -24,13 +24,13 @@ contract Subway {
     }
     
     function credit() public payable {
-        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        balances[msg.sender] = balances[msg.sender] + msg.value;
     }
     
     function buyTickets(uint256 number) public {
         require(balances[msg.sender] >= (number * ticketPrice), "Not enough money");
-        balances[msg.sender] = balances[msg.sender].sub(number * ticketPrice);
-        ticketsBalances[msg.sender] = ticketsBalances[msg.sender].add(number);
+        balances[msg.sender] = balances[msg.sender] - (number * ticketPrice);
+        ticketsBalances[msg.sender] = ticketsBalances[msg.sender] + number;
     }
     
     function balanceOf() public view returns (uint balance) {
