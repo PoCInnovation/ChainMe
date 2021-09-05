ca9d70ccfd1e45b9af07aa2c55814ffaf572e153
Matéo VIEL <mateo.viel@epitech.eu>
2021-09-05 14:32:43 +0200

    [ADD] added main function to launch tests and inits

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 require('dotenv').config();
-const web3 = require('./tests_and_init/utils/connection');
+const BadAssInit = require('./tests_and_init/levels/badAssInit');
+const opcodesInit = require('./tests_and_init/levels/opcodesInit');
 const ultimateInit = require('./tests_and_init/levels/ultimateInit');
+const opcodesTest = require('./tests_and_init/levels/opcodesTest');
+const ultimateTest = require('./tests_and_init/levels/ultimateTest');
+const badAssTest = require('./tests_and_init/levels/badAssTest');
+
+var init = {
+	"badAss" : BadAssInit,
+	"opcodes" : opcodesInit,
+	"ultimate" : ultimateInit
+};
+
+var test = {
+	"badAss" : badAssTest,
+	"opcodes" : opcodesTest,
+	"ultimate" : ultimateTest
+};
 
 (async () => {
-    //await opcodesInit();
-	//await badAssInit();
-	await ultimateInit();
+	let args = process.argv;
+
+	switch (args[2]) {
+		case "init":
+			await init[args[3]]();
+			break;
+		case "test":
+			await test[args[3]](args[4]);
+			break;
+	}
 })();
